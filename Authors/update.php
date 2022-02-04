bf52c523238eb6796e0dd4ba04d57df6af448351
Aliaksei Blizniuk <58771284+AlekseyBliznyuk@users.noreply.github.com>
2022-02-04 03:01:12 +0300

    Update update.php

--- Authors/update.php
+++ Authors/update.php
@@ -1,5 +1,5 @@
 <?php
-// форма
+    // форма
 echo "<form action='index.php' method='post'>
 <p>Id <input type='hidden' name='id'></p>
 <p>Firstname <input type='text' name='firstname'></p>
@@ -7,16 +7,16 @@ echo "<form action='index.php' method='post'>
 <p>Date_of_birth <input type='text' name='date_of_birth'></p>
 <p>Genre <input type='text' name='genre'></p>
 <input type='submit' name='button' value='Update'></form>";
-//соединение с БД
+    //соединение с БД
 $connect = mysqli_connect("localhost", "root", "Leo2803leo", "literature");
-//полуение данных у БД
+    //получение данных у БД
 if (isset($_POST['button'])) {
     $id = $_POST["id"];
     $firstname = $_POST["firstname"];
     $lastname = $_POST["lastname"];
     $date_of_birth = $_POST["date_of_birth"];
     $genre = $_POST["genre"];
-//обновление
+    //обновление
     $sql = "UPDATE literature.authors_info SET
     `firstname`='$firstname', `lastname`='$lastname', `date_of_birth`='$date_of_birth', `genre` = '$genre' WHERE id=$id";
     $result = mysqli_query($connect, $sql);
